e1eecac725f6653caba4779610bd6f5c363d3b9f
Ian Hattendorf <ian@ianhattendorf.com>
2021-06-09 17:34:26 -0700

    Add node client

---
+++ client.js
@@ -0,0 +1,39 @@
+const https = require('https');
+const fs = require('fs').promises;
+
+const makeRequest = (options) => new Promise((resolve, reject) => {
+  console.log('making request');
+  const req = https.request(options, (res) => {
+    let responseBody = '';
+
+    res.on('data', (chunk) => {
+      responseBody += chunk;
+    });
+
+    res.on('end', () => {
+      resolve(responseBody);
+    });
+
+    res.on('error', (err) => {
+      reject(err);
+    });
+  });
+  req.end();
+});
+
+const main = async () => {
+  const options = {
+    hostname: 'localhost',
+    port: 9443,
+    path: '/',
+    method: 'GET',
+    ca: await fs.readFile('./openssl-certs/ca.crt')
+  };
+
+  for (let i = 0; i < 4; ++i) {
+    const response = await makeRequest(options);
+    console.log({ response });
+  }
+};
+
+main();
